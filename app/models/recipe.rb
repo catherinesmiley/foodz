ff34b940c7f677c88e47ae34ac58d8ce2d5ee36a
catherinesmiley <catherinesmiley@gmail.com>
2023-08-25 19:05:47 -0400

    establish associations between Recipe and RecipeStep models

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,7 @@
 class Recipe < ApplicationRecord
     has_many :recipe_ingredients, dependent: :destroy
     has_many :ingredients, through: :recipe_ingredients
+    has_many :recipe_steps, dependent: :destroy
 
     validates :title, presence: true
 end
--- app/models/recipe_step.rb
+++ app/models/recipe_step.rb
@@ -1,3 +1,7 @@
 class RecipeStep < ApplicationRecord
   belongs_to :recipe
+
+  # display error messages when trying to save invalid data
+  validates :instruction, presence: true
+  validates :step_number, uniqueness: { scope: :recipe_id, message: "should be unique per recipe" }
 end
