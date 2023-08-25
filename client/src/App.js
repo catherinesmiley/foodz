52d2d9771fc790f86912da4fe1ab769c65fbe54c
catherinesmiley <catherinesmiley@gmail.com>
2023-08-25 18:31:43 -0400

    implement react-router-dom with Routes and Route components in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home'
 import Navbar from './components/Navbar'
@@ -6,12 +7,17 @@ import Navbar from './components/Navbar'
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <Navbar />
-          <Home />
+      <Router>
+        <div className="App">
+          <div className="App-header">
+            <Navbar />
+              <Routes>
+                  <Route exact path="/" component={Home} />
+              </Routes>
+            <Home />
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -9,18 +9,14 @@ export default function Navbar() {
                     to="/"
                     exact
                 >Home</NavLink></li>
-                <br />
                 <li><NavLink 
                     to="/recipes"
                     exact
                 >All Recipes</NavLink></li>
-                <br />
                 <li><NavLink
                     to="/recipes/new"
                     exact
                 >Create New Recipe</NavLink></li>
-                <br />
-                <br />
             </ul>
     </div>
   )
