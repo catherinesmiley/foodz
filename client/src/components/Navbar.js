91fdc157ad97c6b98e61f72167d4f44a2eea4858
catherinesmiley <catherinesmiley@gmail.com>
2023-08-25 18:35:53 -0400

    add navbar styling

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,20 +4,20 @@ import { NavLink } from 'react-router-dom'
 export default function Navbar() {
   return (
     <div>
-        <ul className="nav">
-                <li><NavLink
-                    to="/"
-                    exact
-                >Home</NavLink></li>
-                <li><NavLink 
-                    to="/recipes"
-                    exact
-                >All Recipes</NavLink></li>
-                <li><NavLink
-                    to="/recipes/new"
-                    exact
-                >Create New Recipe</NavLink></li>
-            </ul>
+      <ul className="nav">
+        <li><NavLink
+            to="/"
+            exact
+        >Home</NavLink></li>
+        <li><NavLink 
+            to="/recipes"
+            exact
+        >All Recipes</NavLink></li>
+        <li><NavLink
+            to="/recipes/new"
+            exact
+        >Create New Recipe</NavLink></li>
+      </ul>
     </div>
   )
 }
