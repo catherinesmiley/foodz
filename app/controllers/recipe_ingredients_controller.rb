eea598157d3902e1d684ddd1980ba9c6d53582d5
catherinesmiley <catherinesmiley@gmail.com>
2023-08-25 20:41:21 -0400

    add RecipeIngredientsController index action

--- app/controllers/recipe_ingredients_controller.rb
+++ app/controllers/recipe_ingredients_controller.rb
@@ -1,2 +1,10 @@
-class RecipeIngredientsController < ApplicationController
+module  Api 
+    module V1
+        class RecipeIngredientsController < ApplicationController
+            def index 
+                @recipes = Recipe.all
+                render json: @recipes 
+            end 
+        end
+    end
 end
