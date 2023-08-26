b24f30299edb9d391caa8f8b9ee6472401daadb9
catherinesmiley <catherinesmiley@gmail.com>
2023-08-25 20:39:19 -0400

    add API namespace to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      resources :recipes, only: [:index]
+    end
+  end  
+  
   resources :recipe_steps
   resources :recipe_ingredients
   resources :ingredients
   resources :recipes
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 end
